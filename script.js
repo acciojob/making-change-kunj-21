1189433da696a5996e6ab2a54f7d93eae243e89a
kunj-21 <107642217+kunj-21@users.noreply.github.com>
2024-05-25 16:05:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const makeChange = (c) => {
   // your name here
+	 const values = {
+        q: 25, // quarter
+        d: 10, // dime
+        n: 5,  // nickel
+        p: 1   // penny
+    };
+    
+    
+    let change = {
+        q: 0,
+        d: 0,
+        n: 0,
+        p: 0
+    };
+
+
+    change.q = Math.floor(c / values.q);
+    c %= values.q;
+
+    change.d = Math.floor(c / values.d);
+    c %= values.d;
+
+    change.n = Math.floor(c / values.n);
+    c %= values.n;
+
+    change.p = c; 
+
+    return change;
 };
 
 // Do not the change the code below
